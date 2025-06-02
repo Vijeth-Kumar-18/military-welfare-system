fec4f0c1f668729bf7b65a5b5551124ad598e79b
YGPavan <ygpavan88@gmail.com>
2025-06-02 23:05:26 +0530

    grievance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import SchemesPage from "./pages/SchemesPage";
-import FamilyGrievancePage from './pages/FamilyGrievancePage';
 import EmergencyPage from "./pages/EmergencyPage";
 import GrievancePage from "./pages/GrievancePage";
-
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import AboutPage from "./pages/AboutPage";
@@ -21,10 +19,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/schemes" element={<SchemesPage />} />
-        <Route path="/family/grievance" element={<FamilyGrievancePage />} /> {/* ✅ Step 2: Add the route */}
         <Route path="/emergency" element={<EmergencyPage />} />
         <Route path="/grievance" element={<GrievancePage />} />
-       
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/about" element={<AboutPage />} />
