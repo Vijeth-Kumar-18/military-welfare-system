8ef973e9effbf6ebd34265062448520d7ad0cc6a
Divan D Shetty <dioneshetty@gmail.com>
2025-06-02 20:01:38 +0530

    updated navbar and schemes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
 
-const App = () => {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SchemesPage from "./pages/SchemesPage";
+import EmergencyPage from "./pages/EmergencyPage";
+import GrievancePage from "./pages/GrievancePage";
+import HelpSupportPage from "./pages/HelpSupportPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+
+function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/schemes" element={<SchemesPage />} />
+        <Route path="/emergency" element={<EmergencyPage />} />
+        <Route path="/grievance" element={<GrievancePage />} />
+        <Route path="/help-support" element={<HelpSupportPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,44 +1,52 @@
-// src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.jpeg';  // Make sure logo.png is in src/assets/
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Navbar.css';
+import logo from '../assets/logo.jpeg';
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-          <img src={logo} alt="Project Logo" width="200" height="150" />
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img src={logo} alt="Logo" width="40" height="40" className="d-inline-block align-text-top me-2" />
+          <span>Military Welfare System</span>
         </Link>
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
-          aria-controls="navbarNav" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon" />
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/schemes">Schemes</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
+              <Link className="nav-link" to="/emergency">Emergency</Link>
             </li>
-            {/* Add more nav links as needed */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/grievance">Grievance</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/support">Help & Support</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-danger" to="/logout">Logout</Link>
+            </li>
           </ul>
         </div>
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
